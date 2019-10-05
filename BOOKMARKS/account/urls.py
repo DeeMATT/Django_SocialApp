8f70c5a40792ad9b470195ab1d4e72d92fd10b0c
DeeMATT <mattode@outlook.com>
2019-10-05 13:36:14 +0100

    Password change view, url and template

--- BOOKMARKS/account/urls.py
+++ BOOKMARKS/account/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
 
-    # change 
+    # change password urls
+    path('password_change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'),
+    path('password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
 ]
